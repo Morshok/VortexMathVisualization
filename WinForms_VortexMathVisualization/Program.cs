fe4f59e03d16d6d4ff390cb40fb3fad3be680800
Morshok <78026935+Morshok@users.noreply.github.com>
2022-09-20 01:11:33 +0200

    Implemented basic design

--- WinForms_VortexMathVisualization/Program.cs
+++ WinForms_VortexMathVisualization/Program.cs
@@ -16,7 +16,7 @@ namespace WinForms_VortexMathVisualization
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new VortexMathVisualizerForm());
         }
     }
 }
