a713601e74ec36ba22f49456627318de010f2f60
Morshok <78026935+Morshok@users.noreply.github.com>
2022-09-20 20:38:02 +0200

    Added function in Utils.cs for finding all loops

--- WinForms_VortexMathVisualization/Utilities/Utils.cs
+++ WinForms_VortexMathVisualization/Utilities/Utils.cs
@@ -8,26 +8,47 @@ namespace WinForms_VortexMathVisualization.Utilities
 {
     internal class Utils
     {
-        public static Point[] GetPointsOnCircle(int centerPoint,int radius, int modulo)
+        public static List<Point> GetPointsOnCircle(int centerPoint,int radius, int modulo)
         {
-            Point[] points = new Point[radius];
+            List<Point> points = new List<Point>();
 
             float circleArcRadius = (float)((2 * Math.PI) / modulo);
             for(int i = 0; i < modulo; i++)
             {
                 float x = (float)(radius * Math.Cos(i * circleArcRadius - Math.PI / 2));
                 float y = (float)(radius * Math.Sin(i * circleArcRadius - Math.PI / 2));
-                points[i] = new Point(centerPoint + x, centerPoint+ y);
+                points.Add(new Point(centerPoint + x, centerPoint+ y));
             }
 
             return points;
         }
 
-        public static int[] GetLoops(int multiplier, int modulo)
+        public static List<int[]> GetLoops(int multiplier, int modulo)
         {
-            
+            List<int[]> loops = new List<int[]>();
+            Stack<int> visited = new Stack<int>();
 
-            return new int[modulo];
+            for(int i = 0; i < modulo; i++)
+            {
+                if (visited.Contains(i)) continue;
+
+                visited.Push(i);
+
+                List<int> loop_i = new List<int>();
+                int u = i;
+
+                while(true)
+                {
+                    u = (u * multiplier) % modulo;
+                    loop_i.Add(u);
+                    if(visited.Contains(u)) break;
+                    visited.Push(u);
+                }
+
+                loops.Add(loop_i.ToArray());
+            }
+
+            return loops;
         }
     }
 }
