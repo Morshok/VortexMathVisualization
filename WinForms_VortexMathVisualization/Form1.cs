91f5337937c547495234d3f7812216cb07b0c04e
Morshok <78026935+Morshok@users.noreply.github.com>
2022-09-20 20:48:47 +0200

    Added exception handling for the input parameters

--- WinForms_VortexMathVisualization/Form1.cs
+++ WinForms_VortexMathVisualization/Form1.cs
@@ -20,8 +20,24 @@ namespace WinForms_VortexMathVisualization
 
         private void visualizeButton_Click(object sender, EventArgs e)
         {
-            int multiplier = int.Parse(multiplierTextBox.Text);
-            int modulo = int.Parse(multiplierTextBox.Text);
+            int multiplier, modulo;
+
+            if(int.TryParse(multiplierTextBox.Text, out multiplier) && int.TryParse(moduloTextBox.Text, out modulo))
+            {
+                // Execute code here
+            }
+            else
+            {
+                bool multiplierParsedCorrectly =  int.TryParse(multiplierTextBox.Text, out multiplier);
+                bool moduloParsedCorrectly = int.TryParse(moduloTextBox.Text, out modulo);
+
+                string message = string.Empty;
+                if (!multiplierParsedCorrectly) message += "Please let multiplier be an integer... \n";
+                if (!moduloParsedCorrectly) message += "Please let modulo be an integer... \n";
+
+                string title = "Warning!";
+                MessageBox.Show(message, title, MessageBoxButtons.OK);
+            }
         }
     }
 }
